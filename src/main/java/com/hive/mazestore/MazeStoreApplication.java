60253d0ef0ce07e2fa6034a8c5c9806bdf3d98b0
vdella <dellatsantos@gmail.com>
2025-12-19 16:56:01 -0300

    added IntelliJ structure for Gradle/SpringBoot

---
+++ src/main/java/com/hive/mazestore/MazeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.hive.mazestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazeStoreApplication.class, args);
+    }
+
+}
