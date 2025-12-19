8a3013c20a24fbc1ae559d54bea41652b3f8ca2a
vdella <dellatsantos@gmail.com>
2025-12-19 17:12:12 -0300

    changed group

---
+++ src/test/java/com/vdella/mazestore/MazeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdella.mazestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
